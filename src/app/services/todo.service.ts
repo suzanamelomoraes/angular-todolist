40b1476078a2c1b4e7b46b04978aa783d97cae30
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-07-02 15:42:46 +1200

    import http client and headers

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,29 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TodoService {
+  constructor() {}
 
-  constructor() { }
-
-  getTodos(){
-      return [
-        {
-          id: 1,
-          title: 'Todo One',
-          completed: false,
-        },
-        {
-          id: 2,
-          title: 'Todo Two',
-          completed: true,
-        },
-        {
-          id: 3,
-          title: 'Todo Three',
-          completed: false,
-        },
-      ];
+  getTodos() {
+    return [
+      {
+        id: 1,
+        title: 'Todo One',
+        completed: false,
+      },
+      {
+        id: 2,
+        title: 'Todo Two',
+        completed: true,
+      },
+      {
+        id: 3,
+        title: 'Todo Three',
+        completed: false,
+      },
+    ];
   }
 }
