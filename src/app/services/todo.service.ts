0b564d1fed2d50c7e93d5781d8e7fb5802328ed3
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-07-02 16:39:57 +1200

    create a method to handle when the toggle is marked as completed which receives an observable and return a put method to update data with url, todo and httpOption as argument, also create the const url which needs the todosUrl plus todo.id and finally creates httpOptions wich defines the headers that describes the content type of the application

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -3,6 +3,12 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Todo } from '../models/Todo';
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
+
 @Injectable({
   providedIn: 'root',
 })
@@ -12,7 +18,14 @@ export class TodoService {
 
   constructor(private http: HttpClient) {}
 
+  // Get Todos
   getTodos(): Observable<Todo[]> {
     return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
+  }
+
+  // Toggle Completed
+  toggleCompleted(todo: Todo): Observable<any> {
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.put(url, todo, httpOptions);
   }
 }
