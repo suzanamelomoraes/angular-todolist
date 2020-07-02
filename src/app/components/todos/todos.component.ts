a0f07e060abe2972f5d2d76b31dca4eaa6a111f7
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-07-02 12:45:40 +1200

    import Todo class from models file, create a property called todos to receive this class and create fake data with these properties inside OnInit

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../models/Todo';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.css']
+  styleUrls: ['./todos.component.css'],
 })
 export class TodosComponent implements OnInit {
+  todos: Todo[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.todos = [
+      {
+        id: 1,
+        title: 'Todo One',
+        completed: false,
+      },
+    ];
   }
-
 }
