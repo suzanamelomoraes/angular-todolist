ab816eb573ad6e34f67dec376477641661211139
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-07-02 16:07:38 +1200

    import observable from rxjs and apply it to the getTodo updating the  method to be a observable that returns the data that it gets from the url property, also apply a limit for this todo list received from this external API

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,32 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import {Todo} from '../models/Todo';
+import { Observable } from 'rxjs';
+import { Todo } from '../models/Todo';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
-    todosUrl: 'https://jsonplaceholder.typicode.com/todos';
+  todosUrl: string = 'https://jsonplaceholder.typicode.com/todos';
+  todosLimit = '?_limit=5';
 
   constructor(private http: HttpClient) {}
 
-  getTodos() {
-    return [
-      {
-        id: 1,
-        title: 'Todo One',
-        completed: false,
-      },
-      {
-        id: 2,
-        title: 'Todo Two',
-        completed: true,
-      },
-      {
-        id: 3,
-        title: 'Todo Three',
-        completed: false,
-      },
-    ];
+  getTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
   }
 }
