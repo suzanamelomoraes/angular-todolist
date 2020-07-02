0aff55498d61ca8744ca6cb37449688a700b54ed
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-07-03 11:31:37 +1200

    import FormsModule and inlude it inside imports to be able to use ngModel inside add-todo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,8 +11,14 @@ import { HeaderComponent } from './components/layout/header/header.component';
 import { AddTodoComponent } from './components/add-todo/add-todo.component';
 
 @NgModule({
-  declarations: [AppComponent, TodosComponent, TodoItemComponent, HeaderComponent, AddTodoComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    TodosComponent,
+    TodoItemComponent,
+    HeaderComponent,
+    AddTodoComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
