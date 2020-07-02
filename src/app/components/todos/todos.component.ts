0bf43ed9f68a5e03e4b292c5eb30665603ddc3d6
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-07-03 10:17:26 +1200

    Once the emited deleteTodo from child todo-item component has been catch in the todos html, create a method that manages the deleteTodo by filtering the todos that don't have the same ID as the selected one and return this new array, removing the selected todo

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -17,4 +17,8 @@ export class TodosComponent implements OnInit {
       this.todos = todos;
     });
   }
+
+  deleteTodo(todo: Todo) {
+    this.todos = this.todos.filter((t) => t.id !== todo.id);
+  }
 }
