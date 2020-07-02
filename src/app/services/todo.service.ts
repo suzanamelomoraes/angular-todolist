955c86cf8a9c23cce871eeb3c01f182b4ece85e1
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-07-03 10:50:46 +1200

    create a deleteTodo to delete in the server the selected todo in the UI including a observable which will be subscribed from the deleteTodo method of the todos component

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -23,6 +23,12 @@ export class TodoService {
     return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
   }
 
+  // Delete Todo
+  deleteTodo(todo: Todo): Observable<Todo> {
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.delete<Todo>(url, httpOptions);
+  }
+
   // Toggle Completed
   toggleCompleted(todo: Todo): Observable<any> {
     const url = `${this.todosUrl}/${todo.id}`;
