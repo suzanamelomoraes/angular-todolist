8a88d4d0fa54fb04b38e1a3bbd7845a75cafe6f3
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-07-02 14:50:57 +1200

    create method to manage the styles of the layout of the todo-item if the taks is marked as completed

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -12,4 +12,14 @@ export class TodoItemComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  // Set dynamic classes
+
+  setClasses() {
+    let classes = {
+      todo: true,
+      'is-completed': this.todo.completed,
+    };
+    return classes;
+  }
 }
