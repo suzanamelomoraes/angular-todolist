e818d175454922809aad2d518d9b6d90750060e4
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-07-02 12:48:00 +1200

    include more data inside todo fake data

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -18,6 +18,16 @@ export class TodosComponent implements OnInit {
         title: 'Todo One',
         completed: false,
       },
+      {
+        id: 2,
+        title: 'Todo Two',
+        completed: true,
+      },
+      {
+        id: 3,
+        title: 'Todo Three',
+        completed: false,
+      },
     ];
   }
 }
