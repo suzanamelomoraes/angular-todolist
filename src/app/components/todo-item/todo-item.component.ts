5f4feefa5c3b600c6cea74c0a3172c23c4a54a9a
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-07-02 15:03:46 +1200

    create method that handles the toggle on the checkbox and invert the completed status all the time that the user check the box

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -22,4 +22,12 @@ export class TodoItemComponent implements OnInit {
     };
     return classes;
   }
+
+  onToggle(todo) {
+    todo.completed = !todo.completed;
+  }
+
+  onDelete(todo) {
+    console.log('deleted');
+  }
 }
